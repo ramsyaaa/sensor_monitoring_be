87a02b7e7cc91873bb87cd439fb3564a22da971b
Muhammad Ramdhan Syakirin <projects.ramdhan@gmail.com>
2024-10-28 23:13:57 +0700

    Add Get Single Device and Sensor API

--- modules/device/http/handler.go
+++ modules/device/http/handler.go
@@ -136,3 +136,171 @@ func (h *DeviceHandler) HandleGetDevicesWithSensor(c *fiber.Ctx) error {
 	response := helper.APIResponse("Devices With Sensor Data fetched successfully", http.StatusOK, "OK", devicesResponse)
 	return c.Status(http.StatusOK).JSON(response)
 }
+
+func (h *DeviceHandler) HandleGetSingleDevice(c *fiber.Ctx) error {
+	tlinkAppId := os.Getenv("TLINK_APP_ID")
+	if tlinkAppId == "" {
+		return c.Status(http.StatusUnauthorized).JSON(helper.APIResponse("Unauthorized", http.StatusUnauthorized, "Unauthorized", nil))
+	}
+
+	authHeader := c.Request().Header.Peek("Authorization")
+	if string(authHeader) == "" {
+		return c.Status(http.StatusUnauthorized).JSON(helper.APIResponse("Unauthorized", http.StatusUnauthorized, "Unauthorized", nil))
+	}
+
+	var request map[string]int
+	if err := c.BodyParser(&request); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(helper.APIResponse("Invalid request", http.StatusBadRequest, "Bad Request", nil))
+	}
+	userId := request["userId"]
+	deviceId := request["deviceId"]
+	currPage := request["currPage"]
+	pageSize := request["pageSize"]
+
+	// Hit external API to get devices
+	baseURL := os.Getenv("BASE_URL")
+	reqBody := fmt.Sprintf(`{"userId":%d,"deviceId":%d,"currPage":%d,"pageSize":%d}`, userId, deviceId, currPage, pageSize)
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/device/getSingleDeviceDatas", baseURL), strings.NewReader(reqBody))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("tlinkAppId", tlinkAppId)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", string(authHeader))
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if len(respBody) == 0 {
+		return c.Status(http.StatusOK).JSON(helper.APIResponse("No devices found", http.StatusOK, "OK", nil))
+	}
+	var devicesResponse struct {
+		CurrPage int         `json:"currPage"`
+		Pages    int         `json:"pages"`
+		PageSize int         `json:"pageSize"`
+		RowCount int         `json:"rowCount"`
+		Device   interface{} `json:"device"`
+	}
+	err = json.Unmarshal(respBody, &devicesResponse)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(helper.APIResponse("Failed to unmarshal devices", http.StatusBadRequest, "Bad Request", nil))
+	}
+
+	response := helper.APIResponse("Devices Single Data fetched successfully", http.StatusOK, "OK", devicesResponse)
+	return c.Status(http.StatusOK).JSON(response)
+}
+
+func (h *DeviceHandler) HandleGetSingleSensor(c *fiber.Ctx) error {
+	tlinkAppId := os.Getenv("TLINK_APP_ID")
+	if tlinkAppId == "" {
+		return c.Status(http.StatusUnauthorized).JSON(helper.APIResponse("Unauthorized", http.StatusUnauthorized, "Unauthorized", nil))
+	}
+
+	authHeader := c.Request().Header.Peek("Authorization")
+	if string(authHeader) == "" {
+		return c.Status(http.StatusUnauthorized).JSON(helper.APIResponse("Unauthorized", http.StatusUnauthorized, "Unauthorized", nil))
+	}
+
+	var request map[string]int
+	if err := c.BodyParser(&request); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(helper.APIResponse("Invalid request", http.StatusBadRequest, "Bad Request", nil))
+	}
+	userId := request["userId"]
+	sensorId := request["sensorId"]
+
+	// Hit external API to get devices
+	baseURL := os.Getenv("BASE_URL")
+	reqBody := fmt.Sprintf(`{"userId":%d,"sensorId":%d}`, userId, sensorId)
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/device/getSingleSensorDatas", baseURL), strings.NewReader(reqBody))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("tlinkAppId", tlinkAppId)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", string(authHeader))
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if len(respBody) == 0 {
+		return c.Status(http.StatusOK).JSON(helper.APIResponse("No devices found", http.StatusOK, "OK", nil))
+	}
+	var responseMap map[string]interface{}
+	err = json.Unmarshal(respBody, &responseMap)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(helper.APIResponse("Failed to unmarshal devices", http.StatusBadRequest, "Bad Request", nil))
+	}
+
+	response := helper.APIResponse("Devices Single Data fetched successfully", http.StatusOK, "OK", responseMap)
+	return c.Status(http.StatusOK).JSON(response)
+}
+
+func (h *DeviceHandler) HandleGetSensorHistory(c *fiber.Ctx) error {
+	tlinkAppId := os.Getenv("TLINK_APP_ID")
+	if tlinkAppId == "" {
+		return c.Status(http.StatusUnauthorized).JSON(helper.APIResponse("Unauthorized", http.StatusUnauthorized, "Unauthorized", nil))
+	}
+
+	authHeader := c.Request().Header.Peek("Authorization")
+	if string(authHeader) == "" {
+		return c.Status(http.StatusUnauthorized).JSON(helper.APIResponse("Unauthorized", http.StatusUnauthorized, "Unauthorized", nil))
+	}
+
+	var request map[string]interface{}
+	if err := c.BodyParser(&request); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(helper.APIResponse("Invalid request", http.StatusBadRequest, "Bad Request", nil))
+	}
+	userId := request["userId"]
+	sensorId := request["sensorId"]
+	startDate := request["startDate"]
+	endDate := request["endDate"]
+	pageSize := request["pageSize"]
+	pagingState := request["pagingState"]
+
+	// Hit external API to get devices
+	baseURL := os.Getenv("BASE_URL")
+	reqBody := fmt.Sprintf(`{"userId":%d,"sensorId":%d,"startDate":%d,"endDate":%d,"pageSize":%d,"pagingState":%d}`, userId, sensorId, startDate, endDate, pageSize, pagingState)
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/device/getSensorHistory", baseURL), strings.NewReader(reqBody))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("tlinkAppId", tlinkAppId)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", string(authHeader))
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if len(respBody) == 0 {
+		return c.Status(http.StatusOK).JSON(helper.APIResponse("No devices found", http.StatusOK, "OK", nil))
+	}
+	var responseMap map[string]interface{}
+	err = json.Unmarshal(respBody, &responseMap)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(helper.APIResponse("Failed to unmarshal devices", http.StatusBadRequest, "Bad Request", nil))
+	}
+
+	response := helper.APIResponse("Devices Single Data fetched successfully", http.StatusOK, "OK", responseMap)
+	return c.Status(http.StatusOK).JSON(response)
+}
--- modules/device/http/routes.go
+++ modules/device/http/routes.go
@@ -8,5 +8,8 @@ func DeviceRoutes(app *fiber.App, handler *DeviceHandler) {
 	deviceHandler := NewDeviceHandler()
 
 	app.Post("/get-device", deviceHandler.HandleGetDevices)
+	app.Post("/get-single-device", deviceHandler.HandleGetSingleDevice)
+	app.Post("/get-single-sensor", deviceHandler.HandleGetSingleSensor)
+	app.Post("/get-sensor-history", deviceHandler.HandleGetSensorHistory)
 	app.Post("/get-device/with-sensor", deviceHandler.HandleGetDevicesWithSensor)
 }
